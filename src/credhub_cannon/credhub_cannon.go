5c5e809cf181e784f3baaab8e6cea10fb603ff32
Ashwin Venkatesh <avenkatesh@pivotal.io>
2017-08-07 15:24:27 -0700

    Use realistic JSON credentials in credhub_cannon
    [#149102239] Operator should know latency and throughput for **get** request with 1 instance
    
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- src/credhub_cannon/credhub_cannon.go
+++ src/credhub_cannon/credhub_cannon.go
@@ -46,34 +46,160 @@ func main() {
 func launchSetRequests(rampedRequest *RampedRequest, url string, name string) {
 	rampedRequest.LocalCSV = "/Users/Pivotal/workspace/setPerfResults.csv"
 	requestBody := `{
-  "name": "` + name + `",
+  "name": "/c/p-spring-cloud-services/circuit-breaker/5c9073f9-677b-4eb7-8c95-4b89d66d2890/credential-json",
   "type": "json",
   "value": {
-    "key": "value",
-    "fancy": { "num": 10 }
+  "dashboard": "https://hystrix-cd5e1b33-2989-414c-8d96-ebe456a0905b.diego.example.com.com",
+  "amqp": {
+    "uris": [
+      "amqp://f962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@10.0.3.44/53d2200b-10b3-4e8e-97f6-f3b5c6318e86"
+    ],
+    "uri": "amqp://f962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@10.0.3.44/53d2200b-10b3-4e8e-97f6-f3b5c6318e86",
+    "http_api_uri": "https://f962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@rabbitmq-management.diego.example.com.com/api/",
+    "vhost": "53d2200b-10b3-4e8e-97f6-f3b5c6318e86",
+    "http_api_uris": [
+      "https://f962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@rabbitmq-management.diego.example.com.com/api/"
+    ],
+    "ssl": false,
+    "dashboard_url": "https://rabbitmq-management.diego.example.com.com/#/login/f962c55f-bc96-4765-b642-0683ab67e9a2/p9fivu9u09v58r8db6r2087pt",
+    "password": "p9fivu9u09v58r8db6r2087pt",
+    "protocols": {
+      "stomp": {
+        "uris": [
+          "stomp://f962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@10.0.3.44:61613"
+        ],
+        "vhost": "53d2200b-10b3-4e8e-97f6-f3b5c6318e86",
+        "username": "f962c55f-bc96-4765-b642-0683ab67e9a2",
+        "password": "p9fivu9u09v58r8db6r2087pt",
+        "port": 61613,
+        "host": "10.0.3.44",
+        "hosts": [
+          "10.0.3.44"
+        ],
+        "ssl": false,
+        "uri": "stomp://f962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@10.0.3.44:61613"
+      },
+      "mqtt": {
+        "uris": [
+          "mqtt://53d2200b-10b3-4e8e-97f6-f3b5c6318e86%3Af962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@10.0.3.44:1883"
+        ],
+        "uri": "mqtt://53d2200b-10b3-4e8e-97f6-f3b5c6318e86%3Af962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@10.0.3.44:1883",
+        "ssl": false,
+        "hosts": [
+          "10.0.3.44"
+        ],
+        "host": "10.0.3.44",
+        "port": 1883,
+        "password": "p9fivu9u09v58r8db6r2087pt",
+        "username": "53d2200b-10b3-4e8e-97f6-f3b5c6318e86:f962c55f-bc96-4765-b642-0683ab67e9a2"
+      },
+      "management": {
+        "uris": [
+          "http://f962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@10.0.3.44:15672/api/"
+        ],
+        "path": "/api/",
+        "ssl": false,
+        "hosts": [
+          "10.0.3.44"
+        ],
+        "password": "p9fivu9u09v58r8db6r2087pt",
+        "username": "f962c55f-bc96-4765-b642-0683ab67e9a2",
+        "port": 15672,
+        "host": "10.0.3.44",
+        "uri": "http://f962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@10.0.3.44:15672/api/"
+      },
+      "amqp": {
+        "uris": [
+          "amqp://f962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@10.0.3.44:5672/53d2200b-10b3-4e8e-97f6-f3b5c6318e86"
+        ],
+        "vhost": "53d2200b-10b3-4e8e-97f6-f3b5c6318e86",
+        "username": "f962c55f-bc96-4765-b642-0683ab67e9a2",
+        "password": "p9fivu9u09v58r8db6r2087pt",
+        "port": 5672,
+        "host": "10.0.3.44",
+        "hosts": [
+          "10.0.3.44"
+        ],
+        "ssl": false,
+        "uri": "amqp://f962c55f-bc96-4765-b642-0683ab67e9a2:p9fivu9u09v58r8db6r2087pt@10.0.3.44:5672/53d2200b-10b3-4e8e-97f6-f3b5c6318e86"
+      }
+    },
+    "username": "f962c55f-bc96-4765-b642-0683ab67e9a2",
+    "hostname": "10.0.3.44",
+    "hostnames": [
+      "10.0.3.44"
+    ]
   },
-  "overwrite": false
+  "stream": "https://turbine-cd5e1b33-2989-414c-8d96-ebe456a0905b.diego.example.com.com"
+},
+  "overwrite": true
 }`
 	rampedRequest.FireRequests(url+"/api/v1/data", "PUT", requestBody)
 }
 
 func launchGetRequests(rampedRequest *RampedRequest, url string, name string) {
 	rampedRequest.LocalCSV = "/Users/Pivotal/workspace/getPerfResults.csv"
-	rampedRequest.FireRequests(url+"/api/v1/data?name=/"+name, "GET", "")
+	rampedRequest.FireRequests(url+"/api/v1/data?name=/c/p-spring-cloud-services/circuit-breaker/5c9073f9-677b-4eb7-8c95-4b89d66d2890/credential-json", "GET", "")
 }
 
 func launchInterpolateRequests(rampedRequest *RampedRequest, url string, name string) {
 	rampedRequest.LocalCSV = "/Users/Pivotal/workspace/interpolatePerfResults.csv"
 
 	requestBody := `{
-	"pp-config-server": [
-	  {
-	    "credentials": {
-	      "credhub-ref": "((/` + name + `))"
-	    },
-	    "label": "pp-config-server"
-	  }
-	]
-	}`
+  "p-circuit-breaker-dashboard": [
+    {
+      "tags": [
+        "circuit-breaker",
+        "hystrix-amqp",
+        "spring-cloud"
+      ],
+      "name": "circuit-breaker",
+      "plan": "standard",
+      "provider": null,
+      "label": "p-circuit-breaker-dashboard",
+      "volume_mounts": [],
+      "syslog_drain_url": null,
+      "credentials": {
+        "credhub-ref": "((/c/p-spring-cloud-services/circuit-breaker/5c9073f9-677b-4eb7-8c95-4b89d66d2890/credential-json))"
+      }
+    }
+  ],
+  "p-service-registry": [
+    {
+      "tags": [
+        "eureka",
+        "discovery",
+        "registry",
+        "spring-cloud"
+      ],
+      "name": "service-registry",
+      "plan": "standard",
+      "provider": null,
+      "label": "p-service-registry",
+      "volume_mounts": [],
+      "syslog_drain_url": null,
+      "credentials": {
+        "credhub-ref": "((/c/p-spring-cloud-services/service-registry/5c9073f9-677b-4eb7-8c95-4b89d66d2891/credential-json))"
+      }
+    }
+  ],
+  "p-config-server": [
+    {
+      "tags": [
+        "configuration",
+        "spring-cloud"
+      ],
+      "name": "config-server",
+      "plan": "standard",
+      "provider": null,
+      "label": "p-config-server",
+      "volume_mounts": [],
+      "syslog_drain_url": null,
+      "credentials": {
+        "credhub-ref": "((/c/p-spring-cloud-services/config-server/5c9073f9-677b-4eb7-8c95-4b89d66d2892/credential-json))"
+      }
+    }
+  ]
+}`
 	rampedRequest.FireRequests(url+"/api/v1/interpolate", "POST", requestBody)
 }
