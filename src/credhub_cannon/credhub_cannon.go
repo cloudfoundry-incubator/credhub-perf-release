bfb80a393bb7418a4bda38c15739cc79e2aa0fe4
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-08-18 16:54:48 -0700

    Fix debug statements
    [#149102239] Operator should know latency and throughput for **get** request with 1 instance
    
    Signed-off-by: Edie Beer <ebeer@pivotal.io>

--- src/credhub_cannon/credhub_cannon.go
+++ src/credhub_cannon/credhub_cannon.go
@@ -33,8 +33,8 @@ func main() {
 	}
 
 	fmt.Println("beginning [" + *requestType + "] tests on instance: " + *url)
-	fmt.Printf("concurrency from %v to %v, step by %v", minConcurrent, maxConcurrent, step)
-	fmt.Printf("%v requests per step", numRequests)
+	fmt.Printf("concurrency from %v to %v, step by %v", *minConcurrent, *maxConcurrent, *step)
+	fmt.Printf("%v requests per step", *numRequests)
 
 	rampedRequest := &RampedRequest{*minConcurrent, *maxConcurrent, *step, *numRequests, "", x509Cert, x509Key}
 
